58bf86571717e63451f34f63eee5ed5963ca7fa1
Selim1710 <mdselimhossainsuhag@gmail.com>
2022-05-23 16:19:36 +0600

    update navbar

--- resources/views/website/layouts/home.blade.php
+++ resources/views/website/layouts/home.blade.php
@@ -1,51 +1,81 @@
 @extends('website.master')
 @section('contents')
-<!-- See All Categories Button -->
-<section class="all-categories">
-    <button class="btn w-100 text-white" style="background:#16a085;" type="button" data-toggle="collapse" data-target="#category" aria-expanded="false" aria-controls="collapseExample">
-        See All Categories &rarr;
-    </button>
-    <div class=" collapse category p-lg-1" id="category">
-        @foreach($categories as $category)
-        <div class="btn-group">
-            <a href="#" class="btn btn-success dropdown-toggle m-1 text-uppercase" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                {{ $category->category_name }}
-            </a>
-            @if(!empty($category->subCategories))
-            <div class="dropdown-menu">
-                @foreach($category->subCategories as $subCategory)
-                <a class="dropdown-item" href="{{ route('show.sub.category.product',$subCategory->id) }}">{{ $subCategory->sub_category_name }}</a>
-                @endforeach
+<!-- carousel -->
+<section>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="{{ ('website/images/t1.jpg') }}" alt="First slide">
             </div>
-            @endif
+            <div class="carousel-item">
+                <img class="d-block w-100" src="{{ ('website/images/t2.jpg') }}" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="{{ ('website/images/t3.png') }}" alt="Third slide">
+            </div>
         </div>
-        @endforeach
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
 </section>
-<!-- Feature Category -->
-<section class="featured-Category border">
-    <div class="categoryHeader">
-        <h1>Featured Category</h1>
-        <p>Get Your Desired Product from Featured Category!</p>
-    </div>
-    <div class="container">
-        <div class="row">
-            @foreach($categories as $category)
-            <div class="col-4 col-lg-2 rounded d-flex align-items-stretch">
-                <div class="card">
-                    <a href="{{ route('show.category.product',$category->id) }}">
-                        <div class="cart-img img-fluid text-center m-2">
-                            <img src="{{ asset('/uploads/category/'.$category->image ) }}" alt="" class="img-fluid">
-                            <br>
-                            <p class="text-center">{{ $category->category_name }}</p>
-                        </div>
+<!-- All Categories -->
+<section class="border m-3">
+    <h1 class="text-center p-2">All Categories</h1>
+    <div class="d-flex">
+        <div class="left_column ml-4" style="height: 500px;border:2px solid grey;">
+            <!-- category >> brand -->
+            <h2 class=" text-center mt-2">
+                Select Brand:
+            </h2>
+            <div class="category p-lg-1" id="category">
+                @foreach($categories as $category)
+                <div class="btn-group">
+                    <a href="#" class="btn dropdown-toggle m-1 text-uppercase" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        {{ $category->category_name }}
                     </a>
+                    @if(!empty($category->subCategories))
+                    <div class="dropdown-menu">
+                        @foreach($category->subCategories as $subCategory)
+                        <a class="dropdown-item" href="{{ route('show.sub.category.product',$subCategory->id) }}">{{ $subCategory->sub_category_name }}</a>
+                        @endforeach
+                    </div>
+                    @endif
                 </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
+        <br><br>
+        <!-- category -->
+        <div class="container">
+            <div class="row">
+                @foreach($categories as $category)
+                <div class="col-4 col-lg-2 rounded d-flex align-items-stretch">
+                    <div class="card">
+                        <a href="{{ route('show.category.product',$category->id) }}">
+                            <div class="cart-img img-fluid text-center m-2">
+                                <img src="{{ asset('/uploads/category/'.$category->image ) }}" alt="" class="img-fluid">
+                                <br>
+                                <p class="text-center">{{ $category->category_name }}</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+        <br><br><br><br><br><br>
     </div>
-    <br><br><br><br><br><br>
 </section>
 <section class="featured-Product border">
     <div class="productHeader">
@@ -78,47 +108,5 @@
         <div class="text-center mb-4"><a href="{{ route('website.all.product') }}" class="btn text-white w-25" style="background-color: #e84393;">View All Product</a></div>
     </div>
 </section>
-<!-- Description -->
-<section class="company-descripiton border">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 text-center font-weight-bold mt-4">
-                <h1>BGD Online Limited Services</h1>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12 p-2 text-center mt-4">
-                <h3 class="text-capitalize">web hosting</h3><br>
-                <div class="description">
-                    <article>
-                        BGD Online Limited make registration of web hosting fast, secure, affordable and secure manner. If you are looking to transfer hosting to shared, reseller , vps or dedicated server provider with superb customer support and have a 99.99% uptime.
-                    </article>
-                </div>
-            </div>
-            <div class="col-lg-12 p-2 text-center mt-4">
-                <h3 class="text-capitalize">web development</h3><br>
-                <div class="description">
-                    <article>
-                        BGD Online Limited expertise in web development. We do outsourcing web design and provide hosting services.We developcompany website , ecommerce solution, Content rich CMS web application for the business needs.You find all service in here
-                    </article>
-                </div>
-            </div>
-            <div class="col-lg-12 p-2 text-center mt-4">
-                <h3 class="text-capitalize">internet connectivity </h3><br>
-                <div class="description">
-                    <article>
-                        Internet access is the process that enables individuals and organisations to connect to the Internet using computer terminals, computers, mobile devices, sometimes via computer networks. Once connected to the Internet, users can access Internet services, such as email. </article>
-                </div>
-            </div>
-            <div class="col-lg-12 p-2 text-center mt-4">
-                <h3 class="text-capitalize">Domain Registration </h3><br>
-                <div class="description">
-                    <article>
-                        We provide Bangladeshi .bd or .bangla and the all the popular domain registration services. Our server uptime 99.99% compared to others. We ensure high server uptime with superb support.You find all service in here
-                </div>
-            </div>
-        </div>
-    </div>
-    <br><br><br><br>
-</section>
+
 @endsection
