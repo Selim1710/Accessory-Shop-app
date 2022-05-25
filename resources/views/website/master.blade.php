d0ac92c4264362c48df5f7a98e6b957c15fce341
Selim1710 <mdselimhossainsuhag@gmail.com>
2022-05-25 17:34:50 +0600

    design for mobile and desktop

--- resources/views/website/master.blade.php
+++ resources/views/website/master.blade.php
@@ -11,7 +11,8 @@
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <!-- product -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
+    
 
     <title>bgd online limited</title>
 </head>
--- resources/views/website/partials/header.blade.php
+++ resources/views/website/partials/header.blade.php
@@ -9,51 +9,47 @@
       <div class="container h-100 mt-4">
          <div class="d-flex justify-content-center h-100">
             <form action="#">
-            <div class="searchbar">
-               <input class="search_input" type="text" name="" placeholder="Search...">
-               <a href="#" class="search_icon"><i class="fa fa-search"></i></a>
-            </div>
+               <div class="searchbar">
+                  <input class="search_input" type="text" name="" placeholder="Search...">
+                  <a href="#" class="search_icon"><i class="fa fa-search"></i></a>
+               </div>
             </form>
          </div>
       </div>
-      <a href="#" class="mr-4">
-         <div class="menu_item d-flex">
-            <h5><i class="fa fa-heart fa-lg"></i></h5>
-            <div class="left_info">
-               <span class="badge badge-danger ml-1">0 items</span>
-            </div>
+      <!-- add to cart -->
+      <a href="#" class="menu_item">
+         <div class="d-flex">
+            <i class="fa-solid fa-basket-shopping fa-2xl"></i><span class="badge badge-danger">0 items</span>
          </div>
       </a>
-      <a href="#" class="mr-4">
-         <div class="menu_item d-flex">
-            <i class="fa fa-shopping-basket fa-lg"></i>
-            <div class="left_info">
-               <span class="badge badge-secondary ml-1">0 items</span>
-            </div>
+      <!-- notification -->
+      <a href="#" class="menu_item">
+         <div class="d-flex">
+            <i class="fa-regular fa-bell fa-2xl"></i><span class="badge badge-secondary">0 items</span>
          </div>
       </a>
    </div>
 </div>
 
 <div class="second_nav" id="Fixed_second_nav">
-   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+   <nav class="navbar navbar-expand-xl navbar-dark bg-dark">
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
          <span class="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="collapse navbar-collapse text-center" id="navbarSupportedContent">
          <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-               <a class="nav-link" href="#"><i class="fa fa-home" aria-hidden="true">&nbsp;Home</i><span class="sr-only">(current)</span></a>
+               <a class="nav-link" href="#">Home</a>
             </li>
             <li class="nav-item">
-               <a class="nav-link" href="#"><i class="fa fa-product-hunt" aria-hidden="true">&nbsp;Product</i></a>
+               <a class="nav-link" href="#">Product</a>
             </li>
             <li class="nav-item">
-               <a class="nav-link" href="#"><i class="fa fa-briefcase" aria-hidden="true">&nbsp;Accessories</i></a>
+               <a class="nav-link" href="#">Accessories</a>
             </li>
             <li class="nav-item">
-               <a class="nav-link" href="#"><i class="fa fa-users" aria-hidden="true">&nbsp;Accounts</i></a>
+               <a class="nav-link" href="#">Accounts</a>
             </li>
          </ul>
 
