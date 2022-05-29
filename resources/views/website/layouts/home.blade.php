f3dd861d2589078c508a73478e13955f0a86f7fd
Selim1710 <mdselimhossainsuhag@gmail.com>
2022-05-29 18:31:22 +0600

    clear fix

--- resources/views/website/layouts/home.blade.php
+++ resources/views/website/layouts/home.blade.php
@@ -106,7 +106,7 @@
             </div>
             <!-- not popular -->
             <div class="row mt-5">
-                
+
                 <div class="col-md-3">
                     <div class="my_card">
                         <img class="img-fluid" src="{{ asset('website/images/t1.jpg') }}" alt="Card image cap">
@@ -120,12 +120,12 @@
     </div>
 </section>
 <!-- product -->
-<section class="all_product">
-    <div class="container">
-        <div class="section_header">
-            <h5>Our Product </h5>
-        </div>
+<div class="container">
+    <div class="section_header">
+        <h5>Our Product</h5>
     </div>
+</div>
+<section class="All_product clearfix">
     <div class="filter"> <button class="btn btn-default" type="button" data-toggle="collapse" data-target="#mobile-filter" aria-expanded="true" aria-controls="mobile-filter">Filters<span class="fa fa-filter pl-1"></span></button>
     </div>
     <div id="mobile-filter">
@@ -142,28 +142,9 @@
                 <div class="form-group"> <input type="checkbox" id="healthy"> <label for="healthy">Healthy Breads</label> </div>
             </form>
         </div>
-        <div class="py-2 border-bottom ml-3">
-            <h6 class="font-weight-bold">Accompainments</h6>
-            <div id="orange"><span class="fa fa-minus"></span></div>
-            <form>
-                <div class="form-group"> <input type="checkbox" id="tea"> <label for="tea">Tea Cakes</label> </div>
-                <div class="form-group"> <input type="checkbox" id="cookies"> <label for="cookies">Cookies</label> </div>
-                <div class="form-group"> <input type="checkbox" id="pastries"> <label for="pastries">Pastries</label> </div>
-                <div class="form-group"> <input type="checkbox" id="dough"> <label for="dough">Cookie Dough</label> </div>
-                <div class="form-group"> <input type="checkbox" id="choco"> <label for="choco">Chocolates</label> </div>
-            </form>
-        </div>
-        <div class="py-2 ml-3">
-            <h6 class="font-weight-bold">Top Offers</h6>
-            <div id="orange"><span class="fa fa-minus"></span></div>
-            <form>
-                <div class="form-group"> <input type="checkbox" id="25off"> <label for="25">25% off</label> </div>
-                <div class="form-group"> <input type="checkbox" id="5off"> <label for="5off" id="off">5% off on artisan breads</label> </div>
-            </form>
-        </div>
     </div>
     <!-- Sidebar filter section -->
-    <section id="sidebar">
+    <div id="sidebar">
         <p> Home | <b>All Breads</b></p>
         <div class="border-bottom pb-2 ml-2">
             <h4 id="burgundy">Filters</h4>
@@ -177,113 +158,96 @@
                 <div class="form-group"> <input type="checkbox" id="healthy"> <label for="healthy">Healthy Breads</label> </div>
             </form>
         </div>
-        <div class="py-2 border-bottom ml-3">
-            <h6 class="font-weight-bold">Accompainments</h6>
-            <div id="orange"><span class="fa fa-minus"></span></div>
-            <form>
-                <div class="form-group"> <input type="checkbox" id="tea"> <label for="tea">Tea Cakes</label> </div>
-                <div class="form-group"> <input type="checkbox" id="cookies"> <label for="cookies">Cookies</label> </div>
-                <div class="form-group"> <input type="checkbox" id="pastries"> <label for="pastries">Pastries</label> </div>
-                <div class="form-group"> <input type="checkbox" id="dough"> <label for="dough">Cookie Dough</label> </div>
-                <div class="form-group"> <input type="checkbox" id="choco"> <label for="choco">Chocolates</label> </div>
-            </form>
-        </div>
-        <div class="py-2 ml-3">
-            <h6 class="font-weight-bold">Top Offers</h6>
-            <div id="orange"><span class="fa fa-minus"></span></div>
-            <form>
-                <div class="form-group"> <input type="checkbox" id="25off"> <label for="25">25% off</label> </div>
-                <div class="form-group"> <input type="checkbox" id="5off"> <label for="5off" id="off">5% off on artisan breads</label> </div>
-            </form>
-        </div>
-    </section>
+    </div>
     <!-- products section -->
-    <section id="products">
+    <div id="products">
         <div class="container">
             <div class="d-flex flex-row">
                 <div class="text-muted m-2" id="res">Showing 44 results</div>
                 <div class="ml-auto mr-lg-4">
-                    <div id="sorting" class="border rounded p-1 m-1"> <span class="text-muted">Sort by</span> <select name="sort" id="sort">
+                    <div id="sorting" class="border rounded p-1 m-1"> <span class="text-muted">Sort by</span>
+                        <select name="sort" id="sort">
                             <option value="popularity"><b>Popularity</b></option>
                             <option value="prcie"><b>Price</b></option>
                             <option value="rating"><b>Rating</b></option>
-                        </select> </div>
+                        </select>
+                    </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
-                    <div class="card"> <img class="card-img-top" src="https://images.pexels.com/photos/1775043/pexels-photo-1775043.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500">
+                    <div class="card">
+                        <img class="card-img-top" src="https://images.pexels.com/photos/1775043/pexels-photo-1775043.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500">
                         <div class="card-body">
                             <h5><b>Multi Grain Bread</b> </h5>
                             <div class="d-flex flex-row my-2">
                                 <div class="text-muted">₹110/loaf</div>
                                 <div class="ml-auto"> <button class="border rounded bg-white sign"><span class="fa fa-plus" id="orange"></span></button> <span class="px-sm-1">1 loaf</span> <button class="border rounded bg-white sign"><span class="fa fa-minus" id="orange"></span></button> </div>
-                            </div> <button class="btn w-100 rounded my-2">Add to cart</button>
+                            </div> <button class="btn btn-warning w-100 rounded my-2">Add to cart</button>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
-                    <div class="card"> <img class="card-img-top" src="https://images.pexels.com/photos/3085146/pexels-photo-3085146.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500">
-                        <div class="card-body">
-                            <h5><b>Bagels</b> </h5>
-                            <div class="d-flex flex-row my-2">
-                                <div class="text-muted">₹35/piece</div>
-                                <div class="ml-auto"> <button class="border rounded bg-white sign"><span class="fa fa-plus" id="orange"></span></button> <span class="px-sm-1">1 pc</span> <button class="border rounded bg-white sign"><span class="fa fa-minus" id="orange"></span></button> </div>
-                            </div> <button class="btn w-100 rounded my-2">Add to cart</button>
-                        </div>
+                <!-- new -->
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Services -->
+<section class="service">
+    <h2>
+        Our Services
+    </h2>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="card_content">
+                    <div class="card_img">
+                        <img src="https://placeimg.com/380/230/nature" alt="">
+                        <span>
+                            <h4>heading</h4>
+                        </span>
+                    </div>
+                    <div class="card_desc">
+                        <h3>Heading</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
+                            voluptas totam</p>
+                        <a href="#" class="btn_card">Read</a>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
-                    <div class="card"> <img class="card-img-top" src="https://images.pexels.com/photos/1448665/pexels-photo-1448665.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500">
-                        <div class="card-body">
-                            <h5><b>White Bread</b> </h5>
-                            <div class="d-flex flex-row my-2">
-                                <div class="text-muted">₹80/loaf</div>
-                                <div class="ml-auto"> <button class="border rounded bg-white sign"><span class="fa fa-plus" id="orange"></span></button> <span class="px-sm-1">1 loaf</span> <button class="border rounded bg-white sign"><span class="fa fa-minus" id="orange"></span></button> </div>
-                            </div> <button class="btn w-100 rounded my-2">Add to cart</button>
-                        </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card_content">
+                    <div class="card_img">
+                        <img src="https://placeimg.com/380/230/animals" alt="">
+                        <span>
+                            <h4>heading2</h4>
+                        </span>
+                    </div>
+                    <div class="card_desc">
+                        <h3>Heading2</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
+                            voluptas totam</p>
+                        <a href="#" class="btn_card">Read</a>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
-                    <div class="card"> <img class="card-img-top" src="https://images.pexels.com/photos/461060/pexels-photo-461060.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500">
-                        <div class="card-body">
-                            <h5><b>Baguette</b> </h5>
-                            <div class="d-flex flex-row my-2">
-                                <div class="text-muted">₹160/piece</div>
-                                <div class="ml-auto"> <button class="border rounded bg-white sign"><span class="fa fa-plus" id="orange"></span></button> <span class="px-sm-1">1 pc</span> <button class="border rounded bg-white sign"><span class="fa fa-minus" id="orange"></span></button> </div>
-                            </div> <button class="btn w-100 rounded my-2">Add to cart</button>
-                        </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card_content">
+                    <div class="card_img">
+                        <img src="https://placeimg.com/380/230/tech" alt="">
+                        <span>
+                            <h4>heading3</h4>
+                        </span>
                     </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
-                    <div class="card"> <img class="card-img-top" src="https://images.pexels.com/photos/209206/pexels-photo-209206.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500">
-                        <div class="card-body">
-                            <h5><b>Masala Bun</b> </h5>
-                            <div class="d-flex flex-row my-2">
-                                <div class="text-muted">₹85/piece</div>
-                                <div class="ml-auto"> <button class="border rounded bg-white sign"><span class="fa fa-plus" id="orange"></span></button> <span class="px-sm-1">1 pc</span> <button class="border rounded bg-white sign"><span class="fa fa-minus" id="orange"></span></button> </div>
-                            </div> <button class="btn w-100 rounded my-2">Add to cart</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
-                    <div class="card d-relative"> <img class="card-img-top" src="https://images.pexels.com/photos/3570/morning-breakfast-croissant.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500">
-                        <div class="card-body">
-                            <h5><b>Croissant</b> </h5>
-                            <div class="rounded bg-white discount" id="orange">10% off</div>
-                            <div class="d-flex flex-row my-2">
-                                <div class="text-muted price"><del>₹55</del>₹45/piece</div>
-                                <div class="ml-auto"> <button class="border rounded bg-white sign"><span class="fa fa-plus" id="orange"></span></button> <span>1pc</span> <button class="border rounded bg-white sign"><span class="fa fa-minus" id="orange"></span></button> </div>
-                            </div> <button class="btn w-100 rounded my-2">Add to cart</button>
-                        </div>
+                    <div class="card_desc">
+                        <h3>Heading3</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
+                            voluptas totam</p>
+                        <a href="#" class="btn_card">Read</a>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
-</section>
-<!-- Services -->
-<section>
-
+    </div>
 </section>
 @endsection
